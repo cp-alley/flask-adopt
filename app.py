f8685d26b598dfffdad76d005de4bcce571d870a
Joshua <jhellst@gmail.com>
2023-09-01 12:34:15 -0700

    Added form functionality via wtforms and continued work on flask routes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 from flask import Flask, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-
+from forms.py import AddPetForm
 
 from models import connect_db, Pet
 
@@ -24,8 +24,45 @@ connect_db(app)
 
 toolbar = DebugToolbarExtension(app)
 
-app.get('/')
+@app.get('/')
 def show_home_page():
     pets = Pet.query.all()
 
     return render_template('pets.html', pets=pets)
+
+
+
+
+@app.get('/pets/new')
+def show_add_form():
+    """Show an add form for pets"""
+    return render_template('add_pet.html')
+
+
+@app.route("/pets/add", methods=["GET", "POST"])
+def add_new_pet():
+    """Process the add form, adding a new pet and going back to /pets"""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        available = form.available.data
+
+        return redirect("/")
+
+    else:
+        return render_template(# Add template for form page)
+
+
+
+
+    db.session.add(user)
+    db.session.commit()
+
+    flash('User added!')
+    return redirect('/users')
--- forms.py
+++ forms.py
@@ -1,1 +1,14 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextField, SelectField, BooleanField
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets"""
+
+    name = StringField("Pet Name")
+    species = StringField("Species of Pet")
+    photo_url = StringField("Photo of Pet, if Available")
+    age = SelectField("Age of Pet", choices=[('baby', 'baby'), ('young', 'young'), ('adult', 'adult'), ('senior', 'senior')])
+    notes = StringField("Notes/Comments on pet, if any")
+    available = BooleanField("Is pet currently available to adopt?")
