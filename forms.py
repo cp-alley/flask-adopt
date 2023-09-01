b9b45374a66947da51eb59c0bba880ccfd5c285c
Christopher Alley <calleyviola@gmail.com>
2023-09-01 19:44:06 -0400

    Tinker with url validation

--- forms.py
+++ forms.py
@@ -8,20 +8,25 @@ class AddPetForm(FlaskForm):
     """Form for adding a pet"""
 
     name = StringField("Pet Name", validators=[InputRequired()])
+
     species = StringField("Species of Pet",
                           validators=[InputRequired(),
                                       AnyOf(['cat', 'dog', 'porcupine'])])
+
     photo_url = StringField("Photo of Pet, if Available",
-                            validators=[URL(require_tld=False), Optional()])
+                            validators=[URL(require_tld=True), Optional()])
+
     age = SelectField("Age of Pet",
                       choices=[('baby', 'baby'),
                                ('young', 'young'),
                                ('adult', 'adult'),
                                ('senior', 'senior')],
                       validators=[InputRequired(),
-                                  AnyOf(['baby', 'young', 'adult', 'senior']
-                      )])
+                                  AnyOf(['baby', 'young', 'adult', 'senior'])]
+                    )
+
     notes = TextAreaField("Notes/Comments on pet, if any", validators=[Optional()])
+
     available = BooleanField("Is pet currently available to adopt?",
                              validators=[Optional()])
 
