30c3bba43bc466be92f7b730c1129c066203fd58
Christopher Alley <calleyviola@gmail.com>
2023-09-01 18:24:35 -0400

    Add validation to add pet form

--- forms.py
+++ forms.py
@@ -1,14 +1,26 @@
 """Forms for adopt app."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SelectField, BooleanField
+from wtforms import StringField, SelectField, BooleanField
+from wtforms.validators import InputRequired, Optional, URL, AnyOf
 
 class AddPetForm(FlaskForm):
     """Form for adding pets"""
 
-    name = StringField("Pet Name")
-    species = StringField("Species of Pet")
-    photo_url = StringField("Photo of Pet, if Available")
-    age = SelectField("Age of Pet", choices=[('baby', 'baby'), ('young', 'young'), ('adult', 'adult'), ('senior', 'senior')])
-    notes = StringField("Notes/Comments on pet, if any")
-    available = BooleanField("Is pet currently available to adopt?")
+    name = StringField("Pet Name", validators=[InputRequired()])
+    species = StringField("Species of Pet",
+                          validators=[InputRequired(),
+                                      AnyOf(['cat', 'dog', 'porcupine'])])
+    photo_url = StringField("Photo of Pet, if Available",
+                            validators=[URL(require_tld=False), Optional()])
+    age = SelectField("Age of Pet",
+                      choices=[('baby', 'baby'),
+                               ('young', 'young'),
+                               ('adult', 'adult'),
+                               ('senior', 'senior')],
+                      validators=[InputRequired(),
+                                  AnyOf(['baby', 'young', 'adult', 'senior']
+                      )])
+    notes = StringField("Notes/Comments on pet, if any", validators=[Optional()])
+    available = BooleanField("Is pet currently available to adopt?",
+                             validators=[Optional()])
