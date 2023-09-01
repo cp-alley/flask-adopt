68ba8d141b18727ddb7f35b54fdf138bb180cd15
Christopher Alley <calleyviola@gmail.com>
2023-09-01 15:01:05 -0400

    Add pet model class

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+DEFAULT_IMAGE_URL = ""
 
 def connect_db(app):
     """Connect this database to provided Flask app.
@@ -14,3 +15,39 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    species = db.Column(
+        db.String(30),
+        nullable=False)
+
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default=DEFAULT_IMAGE_URL)
+
+    age = db.Column(
+        db.String(10),
+        nullable=False)
+
+    notes = db.Column(
+        db.Text,
+        nullable=True
+    )
+
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True
+    )
