4008d412a153e8ef397dbaa79a7083b65adbd839
Joshua <jhellst@gmail.com>
2023-09-01 15:56:18 -0700

    Added additional form for EditPetForm

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 """Forms for adopt app."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, BooleanField
+from wtforms import StringField, SelectField, BooleanField, TextAreaField
 from wtforms.validators import InputRequired, Optional, URL, AnyOf
 
 class AddPetForm(FlaskForm):
-    """Form for adding pets"""
+    """Form for adding a pet"""
 
     name = StringField("Pet Name", validators=[InputRequired()])
     species = StringField("Species of Pet",
@@ -21,6 +21,18 @@ class AddPetForm(FlaskForm):
                       validators=[InputRequired(),
                                   AnyOf(['baby', 'young', 'adult', 'senior']
                       )])
-    notes = StringField("Notes/Comments on pet, if any", validators=[Optional()])
+    notes = TextAreaField("Notes/Comments on pet, if any", validators=[Optional()])
+    available = BooleanField("Is pet currently available to adopt?",
+                             validators=[Optional()])
+
+
+class EditPetForm(FlaskForm):
+    """Form for editing a pet"""
+
+    photo_url = StringField("Photo of Pet, if Available",
+                            validators=[URL(require_tld=False), Optional()])
+
+    notes = TextAreaField("Notes/Comments on pet, if any", validators=[Optional()])
+
     available = BooleanField("Is pet currently available to adopt?",
                              validators=[Optional()])
